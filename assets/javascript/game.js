9b14ba7a112de912b1d954dff8828a6cf0aaadbc
Marcus Price <marcusprice@MacBook-Pro.local>
2018-06-08 15:12:05 -0400

    update2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,25 @@
 // Logic for generating random number between 19-120 to reach//
+var targetNumber = Math.floor(Math.random()* 111 + 19 - 9);
+console.log(targetNumber);
+$("#random-number").append(targetNumber);
 
 //Create on-click event for each diamond //
 
 //On click event will generate random value between 1-12///
+
+    // var Dione = Math.floor(Math.random()* 11 + 1) ;
+    // var Ditwo = Math.floor(Math.random()* 11 + 1) ;
+    // var Dithree = Math.floor(Math.random()* 11 + 1) ;
+    // var Difour = Math.floor(Math.random()* 11 + 1) ;
+    // console.log(one,two,three,four);
+
+    var diamondScores = {
+        DiOne : Math.floor(Math.random()* 11 + 1),
+        DiTwo : Math.floor(Math.random()* 11 + 1),
+        DiThree : Math.floor(Math.random()* 11 + 1),
+        DiFour : Math.floor(Math.random()* 11 + 1)   
+    }
+    console.log(diamondScores)
 
 //On click event will populate output to the total score///
 
@@ -14,7 +31,11 @@
 
 //////Variables ///////
 
-var targetNumber = randomNumber()
+// var targetNumber = Math.floor(Math.random()* 111 + 19 - 9);
+var Dione = Math.floor(Math.random()* 11 + 1) ;
+var Ditwo = Math.floor(Math.random()* 11 + 1) ;
+var Dithree = Math.floor(Math.random()* 11 + 1) ;
+var Difour = Math.floor(Math.random()* 11 + 1) ;
 var totalScore = []
 var Wins = 0;
 var Losses = 0;
