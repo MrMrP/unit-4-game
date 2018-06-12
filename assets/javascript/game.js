52ac99c49f8a57f104e04d253619ac14c0a0261c
Marcus Price <marcusprice@MacBook-Pro.local>
2018-06-12 11:05:13 -0400

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,49 @@
-// Logic for generating random number between 19-120 to reach//
-var targetNumber = Math.floor(Math.random()* 111 + 19 - 9);
+//////Variables ///////
+
+// var targetNumber = Math.floor(Math.random()* 111 + 19 - 9);
+var totalScore = 0
+var Wins = 0;
+var Losses = 0;
+var randomNumber = [];
+
+// Logic for generating random number to reach between 19-120 to reach and displaying number on page for player to see//
+var targetNumber = Math.floor(Math.random()* 110 + 19 - 9);
 console.log(targetNumber);
 $("#random-number").append(targetNumber);
 
-//Create on-click event for each diamond //
+// Logic for generating random number between 1-12 to for randomNumber Array
+var t= 4
+for(var i = 0; i < t; i++) {
+    randomNumber.push(Math.floor(Math.random()* 11 + 1))
+}
+console.log(randomNumber);
 
-//On click event will generate random value between 1-12///
+//Create on-click event for each diamond; on-click each Diamond will pull a number from the randomNumber Array //
 
-    // var Dione = Math.floor(Math.random()* 11 + 1) ;
-    // var Ditwo = Math.floor(Math.random()* 11 + 1) ;
-    // var Dithree = Math.floor(Math.random()* 11 + 1) ;
-    // var Difour = Math.floor(Math.random()* 11 + 1) ;
-    // console.log(one,two,three,four);
-
-    var diamondScores = {
-        DiOne : Math.floor(Math.random()* 11 + 1),
-        DiTwo : Math.floor(Math.random()* 11 + 1),
-        DiThree : Math.floor(Math.random()* 11 + 1),
-        DiFour : Math.floor(Math.random()* 11 + 1)   
-    }
-    console.log(diamondScores)
+$("#one").on("click",function() {
+    var Dione = randomNumber[0];
+    totalScore = totalScore + randomNumber[0];
+    console.log(Dione);
+    console.log(totalScore)
+})  
+$("#two").on("click",function() {
+    var Ditwo = randomNumber[1]
+    totalScore = totalScore + randomNumber[1];
+    console.log(Ditwo);
+    console.log(totalScore)
+})
+$("#three").on("click",function() {
+    var Dithree = randomNumber[2]
+    totalScore = totalScore + randomNumber[2]
+    console.log(Dithree);
+    console.log(totalScore)
+})
+$("#four").on("click",function() {
+    var Difour = randomNumber[3]
+    totalScore = totalScore + randomNumber[3]
+    console.log(Difour);
+    console.log(totalScore)
+})
 
 //On click event will populate output to the total score///
 
@@ -29,35 +53,23 @@ $("#random-number").append(targetNumber);
 
 
 
-//////Variables ///////
-
-// var targetNumber = Math.floor(Math.random()* 111 + 19 - 9);
-var Dione = Math.floor(Math.random()* 11 + 1) ;
-var Ditwo = Math.floor(Math.random()* 11 + 1) ;
-var Dithree = Math.floor(Math.random()* 11 + 1) ;
-var Difour = Math.floor(Math.random()* 11 + 1) ;
-var totalScore = []
-var Wins = 0;
-var Losses = 0;
 
 //////Functions ///////
 
-function randomNumber() {
+// function randomNumber() {
 
-};
+// };
 
 function scores() {
-    
-};
 
 function updateWins() {
     if (totalScore===targetNumber) {
     document.querySelector("#Wins").innerHTML = "Wins:" + Wins++;
     }
-}
+};
 
 function updateLosses() {
     if (totalScore>targetNumber) {
         document.querySelector("#Losses").innerHTML = "Losses:" + Losses++;
         }
-}
+}}
