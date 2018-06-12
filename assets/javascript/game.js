9238a17eb25b0e4443c655626ba993c5bacf3b16
Marcus Price <marcusprice@MacBook-Pro.local>
2018-06-12 12:52:27 -0400

    updating

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,11 @@
 //////Variables ///////
-
 // var targetNumber = Math.floor(Math.random()* 111 + 19 - 9);
+function game (){
 var totalScore = 0
 var Wins = 0;
 var Losses = 0;
 var randomNumber = [];
+
 
 // Logic for generating random number to reach between 19-120 to reach and displaying number on page for player to see//
 var targetNumber = Math.floor(Math.random()* 110 + 19 - 9);
@@ -18,58 +19,50 @@ for(var i = 0; i < t; i++) {
 }
 console.log(randomNumber);
 
-//Create on-click event for each diamond; on-click each Diamond will pull a number from the randomNumber Array //
-
+//Create on-click event for each diamond; on-click each Diamond will pull a number from the randomNumber Array and add total score //
+        
 $("#one").on("click",function() {
     var Dione = randomNumber[0];
     totalScore = totalScore + randomNumber[0];
+    $("#score").text(totalScore);
     console.log(Dione);
     console.log(totalScore)
 })  
 $("#two").on("click",function() {
-    var Ditwo = randomNumber[1]
+    var Ditwo = randomNumber[1];
     totalScore = totalScore + randomNumber[1];
+    $("#score").text(totalScore);
     console.log(Ditwo);
     console.log(totalScore)
 })
 $("#three").on("click",function() {
-    var Dithree = randomNumber[2]
-    totalScore = totalScore + randomNumber[2]
+    var Dithree = randomNumber[2];
+    totalScore = totalScore + randomNumber[2];
+    $("#score").text(totalScore);
     console.log(Dithree);
     console.log(totalScore)
 })
 $("#four").on("click",function() {
-    var Difour = randomNumber[3]
-    totalScore = totalScore + randomNumber[3]
+    var Difour = randomNumber[3];
+    totalScore = totalScore + randomNumber[3];
+    $("#score").text(totalScore);
     console.log(Difour);
-    console.log(totalScore)
-})
+    console.log(totalScore);
+});
 
-//On click event will populate output to the total score///
+// function stats() {
+//     if (totalScore ===  targetNumber) {
+//         Wins++;
+//         alert("You Win!")
+//         document.querySelector(".wins").text = "wins: " + Wins++;
+//         }
 
-//If score is = to target score; player wins, Win column updated, game resets and new target number is generated///
-
-//If score is > than target score; player losses, Loss column updated, game resets and new target number is generated///
-
-
-
-
-//////Functions ///////
-
-// function randomNumber() {
-
-// };
-
-function scores() {
-
-function updateWins() {
-    if (totalScore===targetNumber) {
-    document.querySelector("#Wins").innerHTML = "Wins:" + Wins++;
-    }
-};
-
-function updateLosses() {
-    if (totalScore>targetNumber) {
-        document.querySelector("#Losses").innerHTML = "Losses:" + Losses++;
-        }
-}}
+//         else (totalScore > targetNumber) 
+//         Losses++;
+//         alert("You Lose!")
+//         document.querySelector(".losses").text = "losses: " + Losses++;
+    
+//     }};
+  
+}    
+game ()
